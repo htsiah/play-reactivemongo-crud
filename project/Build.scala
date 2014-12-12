73052d72e86b7571561585dd849bd363dc80a453
htsiah <htsiah@hotmail.com>
2014-12-13 00:38:29 +0800

    Initial Creation

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Play.autoImport._
+import PlayKeys._
+import play.twirl.sbt.Import.TwirlKeys
+
+object ApplicationBuild extends Build {
+
+  val appName = "reactivemongo"
+  val appVersion = "1.0"
+    
+  val appDependencies = Seq(
+      "org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.0.akka23"
+  )
+  
+  val main = Project(appName, file(".")).enablePlugins(play.PlayScala).settings(
+    version := appVersion,
+    libraryDependencies ++= appDependencies
+    // routesImport += "se.radley.plugin.salat.Binders._",
+    // TwirlKeys.templateImports += "org.bson.types.ObjectId"
+  )
+
+}
